63d966b0950dc3aa71c7022a7b85897b2bcf7aca
mmarovich <mmarovich@gmail.com>
2018-08-07 20:14:23 -0500

    finished tutorial

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createPost } from '../actions/postActions';
 
-export default class PostForm extends Component {
+class PostForm extends Component {
   constructor(props) {
     super(props);
 
@@ -25,15 +28,7 @@ export default class PostForm extends Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
+    this.props.createPost(post);
   }
 
   render() {
@@ -66,3 +61,9 @@ export default class PostForm extends Component {
     )
   }
 }
+
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(PostForm)
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
-export default class Posts extends Component {
-  constructor(props) {
-    super(props)
+class Posts extends Component {
 
-    this.state = {
-      posts: []
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
     }
   }
 
-  componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }))
-  }
   render() {
-    const postItems = this.state.posts.map((post, i) => {
+    const postItems = this.props.posts.map((post, i) => {
       return (<div key={i}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -31,3 +32,14 @@ export default class Posts extends Component {
   }
 }
 
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts)
